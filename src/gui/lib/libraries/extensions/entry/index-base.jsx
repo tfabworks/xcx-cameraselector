a427efdf3b73d2c20a0e439c275d013be5304a23
Yoshiaki Kawazu <kawazzz@gmail.com>
2024-08-02 17:17:02 +0900

    add support strech3

---
+++ src/gui/lib/libraries/extensions/entry/index-base.jsx
@@ -0,0 +1,34 @@
+import iconURL from './entry-icon.png';
+import insetIconURL from './inset-icon.png';
+import translations from './translations.json';
+
+let formatMessage = messageData => messageData.defaultMessage;
+
+const setFormatter = formatter => {
+    formatMessage = formatter;
+};
+
+const message = (key) => {
+    const id = `${entry.extensionId}.entry.${key}`
+    const defaultMessage = translations[id] || translations.en[id]
+    const description = `${key} of the extension`
+    return formatMessage({id, defaultMessage, description})
+};
+
+const entry = {
+    get name () { return message('name') },
+    get description () { return message('description') },
+    extensionId: 'cameraselector',
+    extensionURL: 'https://tfabworks.github.io/xcx-cameraselector/dist/cameraselector.mjs',
+    collaborator: 'TFabWorks',
+    iconURL,
+    insetIconURL,
+    featured: true,
+    disabled: false,
+    bluetoothRequired: false,
+    internetConnectionRequired: false,
+    helpLink: 'https://tfabworks.github.io/xcx-cameraselector/',
+    translationMap: translations
+};
+
+export {entry, setFormatter};
---
+++ src/gui/lib/libraries/extensions/entry/index-streach3.jsx
@@ -0,0 +1,10 @@
+/**
+ * This is an extension for Strech3
+ */
+import {entry} from './index-base.jsx';
+
+Object.assign(entry, {
+    extensionURL: null
+});
+
+export default entry;
